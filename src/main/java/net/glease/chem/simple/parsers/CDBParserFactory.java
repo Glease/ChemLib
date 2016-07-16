c0d9ef237e66fc0fcadf594ea08663778240a398
Glease <glease.wang@outlook.com>
2016-07-16 16:29:17 +0800

    Add namespace version
    Signed-off-by: Glease <glease.wang@outlook.com>

--- src/main/java/net/glease/chem/simple/parsers/CDBParserFactory.java
+++ src/main/java/net/glease/chem/simple/parsers/CDBParserFactory.java
@@ -22,19 +22,19 @@ public abstract class CDBParserFactory {
 		Map<String, Object> ps = new HashMap<>();
 
 		@Override
-		protected Object getProperty0(String name) {
+		protected Object getProperty0(final String name) {
 			switch (Objects.requireNonNull(name, "name")) {
-			case XML_ENTITY_RESOLVER_NAME:
-				return er;
-			case XML_ERROR_HANDLER_NAME:
-				return eh;
-			default:
-				if (name.startsWith(XML_PROPERTY_PREFIX)) {
-					return ps.get(name);
-				} else if (name.startsWith(XML_FEATURE_PREFIX)) {
-					return fs.get(name);
-				} else
-					throw new Error();
+				case XML_ENTITY_RESOLVER_NAME:
+					return er;
+				case XML_ERROR_HANDLER_NAME:
+					return eh;
+				default:
+					if (name.startsWith(XML_PROPERTY_PREFIX))
+						return ps.get(name);
+					else if (name.startsWith(XML_FEATURE_PREFIX))
+						return fs.get(name);
+					else
+						throw new Error();
 			}
 		}
 
@@ -44,36 +44,36 @@ public abstract class CDBParserFactory {
 		}
 
 		@Override
-		public boolean isPropertySupported(String name) {
-			return XML_ENTITY_RESOLVER_NAME.equals(Objects.requireNonNull(name)) 
+		public boolean isPropertySupported(final String name) {
+			return XML_ENTITY_RESOLVER_NAME.equals(Objects.requireNonNull(name))
 					|| XML_FEATURE_PREFIX.equals(name)
-					|| name.startsWith(XML_PROPERTY_PREFIX) 
+					|| name.startsWith(XML_PROPERTY_PREFIX)
 					|| name.startsWith(XML_FEATURE_PREFIX);
 		}
 
 		@Override
-		protected Object newParser0(String language) {
+		protected Object newParser0(final String language) {
 			if ("xml".equalsIgnoreCase(language))
 				return new DefaultParser(this);
 			throw new UnsupportedOperationException("default implementation only support xml");
 		}
 
 		@Override
-		protected void setProperty0(String name, Object value) {
+		protected void setProperty0(final String name, final Object value) {
 			switch (Objects.requireNonNull(name, "name")) {
-			case XML_ENTITY_RESOLVER_NAME:
-				er = (EntityResolver) value;
-				break;
-			case XML_ERROR_HANDLER_NAME:
-				eh = (ErrorHandler) value;
-				break;
-			default:
-				if (name.startsWith(XML_PROPERTY_PREFIX)) {
-					ps.put(name, value);
-				} else if (name.startsWith(XML_FEATURE_PREFIX)) {
-					fs.put(name, (Boolean) value);
-				} else
-					throw new Error();
+				case XML_ENTITY_RESOLVER_NAME:
+					er = (EntityResolver) value;
+					break;
+				case XML_ERROR_HANDLER_NAME:
+					eh = (ErrorHandler) value;
+					break;
+				default:
+					if (name.startsWith(XML_PROPERTY_PREFIX))
+						ps.put(name, value);
+					else if (name.startsWith(XML_FEATURE_PREFIX))
+						fs.put(name, (Boolean) value);
+					else
+						throw new Error();
 			}
 		}
 	}
@@ -97,7 +97,7 @@ public abstract class CDBParserFactory {
 	 * name space prefix when unmarshaling to an XML document.
 	 */
 	public static final String XML_DEFAULT_NAMESPACE_PREFIX = "cdbs";
-	public static final String XML_NAMESPACE = "http://glease.net/chem/simple/DataStructure";
+	public static final String XML_NAMESPACE = "http://glease.net/chem/simple/DataStructure/1.0";
 
 	private static ClassLoader getContextClassLoader() {
 		return Thread.currentThread().getContextClassLoader();
@@ -122,7 +122,7 @@ public abstract class CDBParserFactory {
 	 * @param language
 	 *            language which you want a factory supporting it
 	 */
-	public static CDBParserFactory newInstance(ClassLoader cl, String language) {
+	public static CDBParserFactory newInstance(final ClassLoader cl, final String language) {
 		if (language == null)
 			throw new NullPointerException("language");
 		String l = language.isEmpty() ? LANGUAGE_XML : language;
@@ -135,7 +135,7 @@ public abstract class CDBParserFactory {
 	 * @param language
 	 *            language which you want a factory supporting it
 	 */
-	public static CDBParserFactory newInstance(String language) {
+	public static CDBParserFactory newInstance(final String language) {
 		if (language == null)
 			throw new NullPointerException("language");
 		String l = language.isEmpty() ? LANGUAGE_XML : language;
@@ -146,11 +146,11 @@ public abstract class CDBParserFactory {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param name
 	 *            name of desired factory
 	 */
-	public static CDBParserFactory newInstance(String name, ClassLoader cl) {
+	public static CDBParserFactory newInstance(final String name, final ClassLoader cl) {
 		if (name == null || name.isEmpty())
 			throw new IllegalArgumentException("name");
 		Objects.requireNonNull(cl, "classloader");
@@ -162,7 +162,7 @@ public abstract class CDBParserFactory {
 		}
 	}
 
-	public final Object getProperty(String name) {
+	public final Object getProperty(final String name) {
 		if (isPropertySupported(name))
 			return getProperty0(name);
 		else
@@ -174,7 +174,7 @@ public abstract class CDBParserFactory {
 	/**
 	 * Case insensitive. All non-null and non-empty. Value returned shouldn't be
 	 * changed overtime.
-	 * 
+	 *
 	 * @return
 	 */
 	public abstract Set<String> getSupportedLanguage();
@@ -188,21 +188,21 @@ public abstract class CDBParserFactory {
 	 * <p>
 	 * Default implementation just add given plugin to a internal
 	 * {@link LinkedHashSet}.
-	 * 
+	 *
 	 * @param plugin
 	 *            the plugin to be injected into this {@link CDBParserFactory}.
 	 */
-	public void inject(ParserPlugin plugin) {
+	public void inject(final ParserPlugin plugin) {
 		plugins.add(plugin);
 	}
 
-	public final boolean isLanguageSupported(String name) {
+	public final boolean isLanguageSupported(final String name) {
 		return getSupportedLanguage().contains(name);
 	}
 
 	public abstract boolean isPropertySupported(String name);
 
-	public final Object newParser(String language) {
+	public final Object newParser(final String language) {
 		if (getSupportedLanguage().contains(Objects.requireNonNull(language, "language")))
 			return newParser0(language);
 		throw new IllegalArgumentException("Unsupported language: " + language);
@@ -214,7 +214,7 @@ public abstract class CDBParserFactory {
 		return (XMLChemDatabaseParser) newParser(LANGUAGE_XML);
 	}
 
-	public final void setProperty(String name, Object value) {
+	public final void setProperty(final String name, final Object value) {
 		if (isPropertySupported(name))
 			setProperty0(name, value);
 		else
